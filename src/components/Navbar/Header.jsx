e8301665ec629d3d3099056f023fab5cf858579a
vien1994 <88164187+vthai-90@users.noreply.github.com>
2022-08-02 23:03:49 -0700

    Fix responsive bug, replace img, edit header

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -8,13 +8,13 @@ function Header() {
     // All the following styles will be ran as default
     // until screen size of 640px is reached, then 
     // all the 'sm' styles will be active
-    <div className="header-container-sm flex justify-evenly h-24 w-screen bg-white border-gray-300 border-b-2">
+    <div className="header-container-sm flex justify-evenly h-24 w-screen bg-white border-gray-300 border-b-2 font-sans text-slate-500 overflow-x-hidden">
       <div className="sm:hidden self-center p-4 font-medium cursor-pointer">Dropdown</div>    
-     <a href="/" className="logo-sm pl-12 text-4xl text-green-500 self-center cursor-pointer" >VeganFresh2U</a>
+      <Link to="/" className="logo-sm text-3xl text-green-500 font-bold self-center cursor-pointer" >VeganFresh2U</Link>
       <div className="hidden sm:flex sm:items-center">
-        <Link to='/about' className="navbar-items-sm">About Us</Link>
-        <Link to="/pricing" className="navbar-items-sm" >Pricing</Link>
-        <Link to="/menu" className="navbar-items-sm">Menu</Link>
+        <Link to='/about' className="navbar-items-sm">ABOUT US</Link>
+        <Link to="/pricing" className="navbar-items-sm" >PRICING</Link>
+        <Link to="/menu" className="navbar-items-sm">MENU</Link>
         <SignIn />
         <HeaderCart />
       </div>
