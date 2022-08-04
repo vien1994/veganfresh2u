e3e804f0b1fb4f47e90f86d786bfba0fbe4c5f62
VTWalker <vinnythai.29@gmail.com>
2022-08-04 13:48:08 -0700

    Fixed Cart/Modal Overlay
    Overlay now displays content properly, fixed conflicting class names

--- src/components/Menu/MenuTabs.jsx
+++ src/components/Menu/MenuTabs.jsx
@@ -18,21 +18,21 @@ function MenuTabs() {
         Certified fresh recipes delivered to your doorstep. 
         Choose from a variety of healthy meals and put your chef's hat on.
       </div>
-      <div className="bloc-tabs">
+      <div className="menu-bloc-tabs">
         
       <button
     //   Sets the clicked tab to the current index (tab 1 2 or 3)
-          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 1 ? "menu-tabs menu-active-tabs" : "menu-tabs"}
           onClick={() => toggleTab(1)}
         >BREAKFASTS</button>
 
         <button
-          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 2 ? "menu-tabs menu-active-tabs" : "menu-tabs"}
           onClick={() => toggleTab(2)}
         >LUNCHES</button>
 
         <button
-          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 3 ? "menu-tabs menu-active-tabs" : "menu-tabs"}
           onClick={() => toggleTab(3)}
         >DINNERS</button>
       </div>
@@ -40,7 +40,7 @@ function MenuTabs() {
         <div className="grow"> {/* The content of the active tab will fill the whole container */}
 
             {/* Checks which tab is active and displays the content of the currently active tab  */}
-            <div className={toggleState === 1 ? "content  active-content" : "content"}>
+            <div className={toggleState === 1 ? "menu-content  menu-active-content" : "menu-content"}>
               <div className="flex justify-center">
                 <h1 className="tab-header inline-flex">Breakfasts
                 <div className="font-normal italic text-base pl-4 pt-1">Available in 2-4 servings</div>
@@ -50,7 +50,7 @@ function MenuTabs() {
                 <DummyMeals />
             </div>
 
-            <div className={toggleState === 2 ? "content  active-content" : "content"}>
+            <div className={toggleState === 2 ? "menu-content  menu-active-content" : "menu-content"}>
               <div className="flex justify-center">
                 <h1 className="tab-header inline-flex">Lunches
                 <div className="font-normal italic text-base pl-4 pt-1">Available in 2-4 servings</div>
@@ -59,7 +59,7 @@ function MenuTabs() {
                 <DummyMeals />
             </div>
 
-            <div className={toggleState === 3 ? "content  active-content" : "content"}>
+            <div className={toggleState === 3 ? "menu-content  menu-active-content" : "menu-content"}>
               <div className="flex justify-center">
                 <h1 className="tab-header inline-flex">Dinners
                 <div className="font-normal italic text-base pl-4 pt-1">Available in 2-4 servings</div>
