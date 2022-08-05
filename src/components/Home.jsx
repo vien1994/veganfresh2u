fc6bf1fec71173510be902449ed52dff303f7971
vien1994 <88164187+vthai-90@users.noreply.github.com>
2022-08-04 21:11:38 -0700

    new wave bg & slight text edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
   return (
     <div className="row-start-1 row-end-2 col-span-full grid grid-cols-7 grid-rows-4">
       {/* <div className="col-start-1 col-end-8 row-start-1 row-end-4 max-h-80 overflow-hidden"></div> */}
-      {/* <img src={wave} className=" h-4/6 w-full col-start-1 col-end-8 row-start-1"/> */}
-      <div className="bg-green-500 h-full w-full col-start-1 col-end-8 row-start-1" />
-      <div className="col-start-3 row-start-1 text-6xl text-gray-800">Test</div>
+      <img src={wave} className="w-full col-start-1 col-end-8 row-start-1 row-end-5 opacity-50 -z-10"/>
+      {/* <div className="bg-green-500 h-full w-full col-start-1 col-end-8 row-start-1" /> */}
+      <div className="col-start-2 row-start-2 text-2xl sm:text-4xl md:text-6xl text-gray-800">Enjoy</div>
     </div> 
   )
 }
