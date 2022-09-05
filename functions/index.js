3b888bb606f63c87aeacc88bb99bc4346c65ada0
vien1994 <88164187+vthai-90@users.noreply.github.com>
2022-09-05 16:39:27 -0700

    create basic test orders to stripe

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require("firebase-functions");
+
+exports.createStripeCheckout = functions.https.onCall(async (data, context) => {
+  const stripe = require("stripe")(functions.config().stripe.secret_key);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http://localhost:3000/orders",
+    cancel_url: "http://localhost:3000/orders",
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "usd",
+          unit_amount: (100) * 100, // 10,000 = $100
+          product_data: {
+            name: "Jackfruit Burger",
+          },
+        },
+      },
+    ],
+  });
+
+  return {
+    id: session.id,
+  };
+});
