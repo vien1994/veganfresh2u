cb2dc17d7ffa89c4d8fadf418b0d94284173ebd2
VTWalker <vinnythai.29@gmail.com>
2022-10-01 10:43:29 -0700

    Basic styling to menu modal

--- src/components/Modal/NewModal.jsx
+++ src/components/Modal/NewModal.jsx
@@ -10,12 +10,12 @@ function NewModal(props) {
     <React.Fragment>
       <div className="h-screen w-screen opacity-70 bg-gray-800 fixed z-10" onClick={() => {showModalHandler(false)}} ></div>
       <div className="fixed flex flex-col h-4/6 w-2/3 z-20 bg-white rounded-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 py-2">
-        <div>IMAGE</div>
-        <h1 className="font-semibold" >{modalData.name}</h1>
-        <p>Item Description. YouTube Link goes here as well</p>
-        <p>Allergy Info{modalData.allergies}</p>
-        <p>Nutrition Info{modalData.nutrition}</p>
-        <button>Add to Cart</button>
+        <div className="border-1 border-black self-center w-full h-2/5" >IMAGE</div>
+        <h1 className="font-semibold text-lg mb-2" >{modalData.name}</h1>
+        <p className="my-3" >Item Description. YouTube Link goes here as well</p>
+        <p className="italic" >Allergy Info{modalData.allergies}</p>
+        <p className="italic" >Nutrition Info{modalData.nutrition}</p>
+        <button className="absolute bottom-0 right-0 m-4" >Add to Cart</button>
       </div>
     </React.Fragment>
   )
