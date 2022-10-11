b5139950a7aef81cf074950c0d11a3751ce21981
vien1994 <88164187+vthai-90@users.noreply.github.com>
2022-10-10 21:58:56 -0700

    Fix profile dropdown after removing Checkout optio

--- src/components/Navbar/ProfileDropdown.jsx
+++ src/components/Navbar/ProfileDropdown.jsx
@@ -7,7 +7,7 @@ export default function ProfileDropdown() {
   const { showCartHandler, auth } = useContext(Context);
 
   return(
-    <ul className="absolute -translate-x-1/3 z-10 border border-gray-300 bg-white">
+    <ul className="absolute -translate-x-1/4 -translate-y-2 z-10 border border-gray-300 bg-white">
       <li className="navbar-profile-dropdown-li">
         <Link to="/profile">PROFILE</Link>
       </li>
